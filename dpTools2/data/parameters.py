85d6a531ba4d2aad0c5119317633cc14ee4326e5
marshall <mmccraw98@gmail.com>
2024-09-09 08:43:42 -0400

    improved parameters object

--- dpTools2/data/parameters.py
+++ dpTools2/data/parameters.py
@@ -1,12 +1,22 @@
 
 class Parameters:
     def __init__(self, filepath):
-        self.data = self.load_parameters(filepath)
+        self.load_parameters(filepath)
 
     def load_parameters(self, filepath):
-        params = {}
         with open(filepath, 'r') as file:
             for line in file:
                 key, value = line.strip().split('\t')
-                params[key] = value
-        return params
+                try:
+                    value = float(value)
+                except:
+                    pass
+                setattr(self, key, value)
+
+    def __repr__(self):
+        variables = {
+            var: repr(getattr(self, var)) for var in dir(self)
+            if not var.startswith('_') and not callable(getattr(self, var))
+        }
+        variables_repr = "\n\t".join(f"{k}={v}" for k, v in variables.items())
+        return f"Parameters(\n\t{variables_repr}\n)"
